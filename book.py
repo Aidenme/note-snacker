24a42576fde71476303118e15bb8ffb8d535ef94
Aidenme <aidenettlinger@gmail.com>
2025-02-24 13:06:50 -0500

    Each book in the library now has the browser sent to it.

--- book.py
+++ book.py
@@ -1,8 +1,12 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
 class Book:
-    def __init__(self, title, author, button):
+    def __init__(self, title, author, button, browser):
         self.title = title
         self.author = author
         self.button = button
+        self.browser = browser
         self.soup = None
 
     def __str__(self):
@@ -18,3 +22,9 @@ class Book:
         else:
             print("Book has its soup!")
             return True
+        
+    def setSoup(self, soup):
+        self.soup = soup
+
+    def getSoup(self, browser):
+        self.soup = BeautifulSoup(browser.html, 'lxml')
